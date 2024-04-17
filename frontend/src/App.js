d8898d7e5129e8d35a64d110d9e0466ff3c3a8ee
Karthick <ganeshkarthik18697@gmail.com>
2024-04-17 13:26:10 +0530

    fixed google Oauth login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,9 +21,11 @@ function App() {
     )
   );
 
+  let clientId = process.env.REACT_APP_CLIENT_ID ;
+
   return (
     <>
-      <GoogleOAuthProvider clientId="319799807729-f4o2e5qr8a1b4innj40v6urlaorrt73a.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId={clientId}>
         <RouterProvider router={router} />
       </GoogleOAuthProvider>
     </>
